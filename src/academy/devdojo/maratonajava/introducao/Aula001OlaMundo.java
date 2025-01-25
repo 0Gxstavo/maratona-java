b46d8723eda633555198f74badb50e1e48c199c1
0Gxstavo <ogu.santos14@gmail.com>
2025-01-25 17:43:48 -0300

    Arquivos iniciais de estudos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula001OlaMundo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ *  isso é um comentário <b>javadoc</b>
+ */
+
+public class Aula001OlaMundo {
+    // isso é um comentário
+
+    /*
+        isso também é um comentário,
+        mas de multiplas linhas
+    */
+    public static void main(String[] args){
+        System.out.println("Ola, Mundo!");
+    }
+}
